3f7b34c4874fac98158e9978ba3fa717428674d6
unknown <chingnokyee@gmail.com>
2020-03-04 22:28:06 +0800

    tiffany Mar04

---
+++ app/src/main/java/com/hku/tripals/model/User.java
@@ -0,0 +1,106 @@
+package com.hku.tripals.model;
+
+public class User {
+    private String uid;
+    private String avatarImageUrl;
+    private String gender;
+    private String birthday;
+    private String homeCountry;
+    private String language;
+    private String bio;
+    //private String interest;
+
+
+    public User() {
+    }
+
+    public User(String uid, String gender, String birthday, String homeCountry, String language, String bio){
+        this.uid = uid;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.homeCountry = homeCountry;
+        this.language = language;
+        this.bio = bio;
+    }
+
+
+    public User(String uid, String gender, String birthday, String homeCountry, String language, String bio, String avatarImageUrl){
+        this.uid = uid;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.homeCountry = homeCountry;
+        this.language = language;
+        this.bio = bio;
+        this.avatarImageUrl = avatarImageUrl;
+    }
+
+    //uid
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    //gender
+
+    public String getgender() {
+        return gender;
+    }
+
+    public void setgender(String uid) {
+        this.gender = gender;
+    }
+
+    //birthday
+
+    public String getbirthday() {
+        return birthday;
+    }
+
+    public void setbirthday(String uid) {
+        this.birthday= birthday;
+    }
+
+    //homeCountry
+
+    public String gethomeCountry() {
+        return homeCountry;
+    }
+
+    public void sethomeCountry(String uid) {
+        this.homeCountry = homeCountry;
+    }
+
+    //language
+
+    public String getlanguage() {
+        return language;
+    }
+
+    public void setlanguage(String uid) {
+        this.language = language;
+    }
+
+    //bio
+
+    public String getbio() {
+        return bio;
+    }
+
+    public void setbio(String uid) {
+        this.language = bio;
+    }
+
+    //AvatarImageUr
+
+    public String getAvatarImageUrl() {
+        return avatarImageUrl;
+    }
+
+    public void setAvatarImageUrl(String avatarImageUrl) {
+        this.avatarImageUrl = avatarImageUrl;
+    }
+}
